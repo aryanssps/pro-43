8cbb5210aa940d2333711a3fb0a967ba9a4484ec
aryanssps <74345307+aryanssps@users.noreply.github.com>
2021-05-09 10:26:03 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("lightBlue")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightGreen")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+    fill("lightBlue")
+    textSize(15)
+    text("C U T N I O F N", 700,150)
+    fill("lightGreen")
+    text("Hint: Performs a particular task !!", 700,170)
+
+    fill("lightBlue")
+    textSize(15)
+    text("A T E A D S B A", 100,250)
+    fill("lightGreen")
+    text("Hint: Stores all information !!", 100,270)
+
+}
